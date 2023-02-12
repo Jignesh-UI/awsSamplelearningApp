81cab9e32960679c872aae41e00809176933b00d
Jignesh Patel <hellojiger@gmail.com>
2023-02-12 17:32:09 +0530

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { FirstComponent } from './component/first/first.component';
+import { SecondComponent } from './component/second/second.component';
+import { ThirdComponent } from './component/third/third.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first', component: FirstComponent },
+  { path: 'second', component: SecondComponent },
+  { path: 'third', component: ThirdComponent },
+  { path: '', component: DashboardComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './component/first/first.component';
+import { SecondComponent } from './component/second/second.component';
+import { ThirdComponent } from './component/third/third.component';
+import { NavComponent } from './shared/component/nav/nav.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent,
+    NavComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
